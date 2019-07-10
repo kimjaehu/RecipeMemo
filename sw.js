56f40117d4b5fce894de4b7530b4dce594ea365c
Jay Kim <kimjaehu@gmail.com>
2019-07-10 14:36:18 -0400

    adds precaching assets

--- sw.js
+++ sw.js
@@ -1,14 +1,33 @@
+const cacheStatic = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/recipe.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
 // service worker: install event
 self.addEventListener('install', e => {
-  console.log('service worker has been installed');
+  // console.log('service worker has been installed');
+  e.waitUntil(
+    caches.open(cacheStatic).then(cache => {
+      console.log('caching shell');
+      cache.addAll(assets);
+    })
+  );
 });
 
 // service worker: activate event
 self.addEventListener('activate', e => {
-  console.log('service worker has been activated');
+  // console.log('service worker has been activated');
 });
 
 // service worker: fetch event
 self.addEventListener('fetch', e => {
-  console.log('fetch event', e);
+  // console.log('fetch event', e);
 });
