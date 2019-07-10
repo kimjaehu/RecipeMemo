df214ed730242a85432d345e13af7bc514c9b393
Jay Kim <kimjaehu@gmail.com>
2019-07-09 23:56:34 -0400

    adds activate event serviceworker

--- sw.js
+++ sw.js
@@ -2,3 +2,8 @@
 self.addEventListener('install', e => {
   console.log('service worker has been installed');
 });
+
+// service worker: activate event
+self.addEventListener('activate', e => {
+  console.log('service worker has been activated');
+});
