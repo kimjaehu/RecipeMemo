6a16d97867b099a2701eaad7ba488920eae35791
Jay Kim <kimjaehu@gmail.com>
2019-07-11 00:04:37 -0400

    adds version control function

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheStatic = 'site-static';
+const cacheStatic = 'site-static-v1';
 const assets = [
   '/',
   '/index.html',
@@ -8,7 +8,8 @@ const assets = [
   '/css/styles.css',
   '/css/materialize.min.css',
   '/img/recipe.png',
-  'https://fonts.googleapis.com/icon?family=Material+Icons'
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 
 // service worker: install event
@@ -24,10 +25,22 @@ self.addEventListener('install', e => {
 
 // service worker: activate event
 self.addEventListener('activate', e => {
-  // console.log('service worker has been activated');
+  console.log('service worker has been activated');
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheStatic).map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 // service worker: fetch event
 self.addEventListener('fetch', e => {
   // console.log('fetch event', e);
+  e.respondWith(
+    caches.match(e.request).then(cacheRes => {
+      return cacheRes || fetch(e.request);
+    })
+  );
 });
