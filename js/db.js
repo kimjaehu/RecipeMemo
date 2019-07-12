767ffb583aeb6c6575500514a6d8a2f0b1659e52
Jay Kim <kimjaehu@gmail.com>
2019-07-12 15:41:39 -0400

    adds indexedDB

--- js/db.js
+++ js/db.js
@@ -1,3 +1,14 @@
+// data persistence
+db.enablePersistence().catch(err => {
+  if (err.code == 'failed-precondition') {
+    // probably multiple tabs open at once
+    console.log('failed: persistence');
+  } else if (err.code == 'unimplemented') {
+    // no browser support
+    console.log('persistence unavailable');
+  }
+});
+
 //real time listener
 db.collection('recipes').onSnapshot(snapshot => {
   console.log(snapshot.docChanges());
@@ -5,7 +16,7 @@ db.collection('recipes').onSnapshot(snapshot => {
     // console.log(change, change.doc.data(), change.doc.id);
     if (change.type === 'added') {
       // add data to web page
-      renderRecipe(change.doc.data(), change.doc.id)
+      renderRecipe(change.doc.data(), change.doc.id);
     }
     if (change.type === 'removed') {
       //remove data from web page
